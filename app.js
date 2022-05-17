b159d767710f0280184c84aa9cf947390fa932a9
Nicolas Veracx <nicover51969@hotmail.fr>
2022-05-17 21:52:29 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const txtAnim = document.querySelector('h1');
+new Typewriter (txtAnim,{
+    loop:true,
+    deleteSpeed:70
+})
+.changeDelay(60)
+.typeString('Hey, je suis un développeur web.')
+.pauseFor(2000)
+.deleteChars(5)
+.typeString('<span style="color:rgb(241, 24, 102);"> frontend.</span>')
+.pauseFor(2000)
+.deleteChars(9)
+.typeString('<span style="color:rgb(251, 81, 30);"> backend.</span>')
+.pauseFor(3000)
+.start()
+
+$(document).ready(function(){
+    $('.diapo').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+});
+
+
+class Carousel{
+    constructor (element, options = {}){
+        /**
+         * @param {HTMLElement}elements
+         * @param {Object}options
+         * @param {Object}options.slidesToScroll Nombre d'élements à faire défiler
+         * @param {Object}options.slidesVisible Nombre d'élements à afficher
+         */
+    }
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    new Carousel(document.querySelector('diapo'),{
+        slideToScroll: 1,
+        slidesVisible: 3
+    });
+})
